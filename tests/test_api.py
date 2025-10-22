6a56f5c8222b3cffd6d520c480d1fb961fb2d7ed
Silvert05 <cochadavid2005@gmail.com>
2025-10-22 16:27:49 -0500

    git

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,12 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
 from app import app
 
 def test_home_route():
